c726cc9da10e9f492a51871af5f729a2bae1e938
Bowen Deng <bowen.deng@uisee.com>
2017-08-25 19:38:28 +0800

    Finish the tensorflow pipeline

--- computer_vision.py
+++ computer_vision.py
@@ -4,11 +4,18 @@ import tensorflow as tf
 import numpy as np
 import os
 
-from tensorflow.contrib import slim
+import matplotlib.pyplot as plt
 
-from models.slim.datasets import imagenet
-from model.slim.nets import inception
-from model.slim.preprocessing import inception_preprocessing
+import urllib.request as urllib
+
+import sys
+sys.path.append(r'/home/radmin/programs/wechat/models/slim') # Change this on your computer
+
+from datasets import imagenet
+from nets import inception
+from preprocessing import inception_preprocessing
+
+from tensorflow.contrib import slim
 
 # Convert the src image to dst image, and optionally return info about the image.
 # Args:
@@ -19,4 +26,33 @@ from model.slim.preprocessing import inception_preprocessing
 def convert_image(src, dst):
   return 'I can see the image.'
 
+image_size = inception.inception_v3.default_image_size
 
+url = 'https://upload.wikimedia.org/wikipedia/commons/7/70/EnglishCockerSpaniel_simon.jpg'
+image_string = urllib.urlopen(url).read()
+image = tf.image.decode_jpeg(image_string, channels=3)
+processed_image = inception_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+processed_images  = tf.expand_dims(processed_image, 0)
+
+with slim.arg_scope(inception.inception_v3_arg_scope()):
+  logits, _ = inception.inception_v3(processed_images, num_classes=1001, is_training=False)
+
+probabilities = tf.nn.softmax(logits)
+
+init_fn = slim.assign_from_checkpoint_fn('pre_trained/inception_v3.ckpt', slim.get_model_variables('InceptionV3'))
+
+with tf.Session() as sess:
+  init_fn(sess)
+  np_image, probabilities = sess.run([image, probabilities])
+  probabilities = probabilities[0, 0:]
+  sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x: x[1])]
+
+plt.figure()
+plt.imshow(np_image.astype(np.uint8))
+plt.axis('off')
+plt.show()
+
+names = imagenet.create_readable_names_for_imagenet_labels()
+for i in range(5):
+  index = sorted_inds[i]
+  print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index]))
