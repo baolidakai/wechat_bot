c3c3915648429d471941d29bac55d04b221e5a27
Bowen Deng <bowen.deng@uisee.com>
2017-08-26 10:55:16 +0800

    Make the auto reply work for both text and image for both private and group chat

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ The current text response is from tuling robot.
 
 Working on image classification
 
-
 ## Prerequisites
 * tensorflow
 * itchat
@@ -14,3 +13,18 @@ Working on image classification
 * scipy
 * Install tensorflow models inside this repository. See https://github.com/tensorflow/models/tree/master/slim.
 * Download http://download.tensorflow.org/models/inception\_v3\_2016\_08\_28.tar.gz and unzip inside pre\_trained
+* Install https://github.com/NikolaiT/GoogleScraper
+* Add a file named data.json which stores something like:
+{
+	"trigger_nicknames": ["邓博文", ...],
+	"trigger_usernames": [
+	  "@14341204faee8b10343b43632b7aa83163b81c64e04825c7df32480d9d16a63a",
+	  ...
+	],
+	"myself_username": "@14341204faee8b10343b43632b7aa83163b81c64e04825c7df32480d9d16a63a"
+}
+
+## TODOs
+* Add notification functionality
+* Use mobilenet+ssd
+* Make the code more efficient
--- main.py
+++ main.py
@@ -4,85 +4,84 @@ import os
 import shutil
 import re
 import tensorflow as tf
+import json
 
 from computer_vision import convert_image
+
+DEBUG = False # Turn on DEBUG to suppress sending message, turn off to enable real message sending
 
 survived = True
 
 KEY = 'ca098ebe818b49df98af997bef29b3b3'
 
-def get_response(msg):
-  if msg == u'你喜欢我吗':
-    return u'{}我喜欢你'.format(msg['ActualNickName'])
-  if msg == u'傻逼滚':
+def get_response(content):
+  if content == u'傻逼滚':
     global survived
     survived = False
     return u'我滚了'
-  if msg == u'傻逼回来':
+  if content == u'傻逼回来':
     global survived
     survived = True
     return '我回来了'
   url = 'http://www.tuling123.com/openapi/api'
   data = {
       'key': KEY,
-      'info': msg,
+      'info': content,
       'userid': 'pth-robot',
   }
-
   try:
     r = requests.post(url, data=data).json()
     return r.get('text')
   except:
     pass
 
-trigger_nicknames = {u'邓博文', u'Bao Qi', u'小七'}
-trigger_usernames = {
-  '@14341204faee8b10343b43632b7aa83163b81c64e04825c7df32480d9d16a63a', # 邓博文
-  '@3a680a3c3bcbb87e139ce2e74726ce24', # BQ
-  '@a4e4472a92099212296e110076c159daf20ca505b9c51c405f09a6dc0655b6ec', # 小七
-  '@bbf211baa963bd4a84ba5b0769d24c82dd50b6911af6d9e9c4d789da8d528711' # 黄益民
-}
-myself_username = '@14341204faee8b10343b43632b7aa83163b81c64e04825c7df32480d9d16a63a'
+user_data = json.load(open('data.json', 'r'))
+trigger_nicknames = user_data['trigger_nicknames']
+trigger_usernames = user_data['trigger_usernames']
+myself_username = user_data['myself_username']
 
+# Return the source user to reply to, none if no need to reply
 def get_source(msg):
-  if msg['ToUserName'].startswith('@@'):
+  if msg['FromUserName'] == myself_username and msg['ToUserName'].startswith('@@'): # my message in a group chat
+    print('my message to a group')
     return msg['ToUserName']
-  if msg['FromUserName'].startswith('@@'):
+  elif msg['FromUserName'].startswith('@@') and msg['ActualNickName'] in trigger_nicknames and msg['ToUserName'] == myself_username: # group chat message sent to me from a whitelist user
+    print('group message to me')
     return msg['FromUserName']
-  elif msg['ToUserName'] == myself_username:
-    return msg['FromUserName']
-  elif msg['FromUserName'] == myself_username:
+  elif msg['FromUserName'] == myself_username and msg['ToUserName'] in trigger_usernames: # my private message to whitelist user
+    print('my message to friend')
     return msg['ToUserName']
+  elif msg['FromUserName'] in trigger_usernames and msg['ToUserName'] == myself_username: # private message from whitelist user to myself
+    print('friend message to me')
+    return msg['FromUserName']
   return None
 
-@itchat.msg_register([itchat.content.TEXT], isGroupChat=True)
+@itchat.msg_register([itchat.content.TEXT], isGroupChat=True, isFriendChat=True)
 def reply_message(msg):
-  if msg['ActualNickName'] not in trigger_nicknames:
-    print('Ignoring this message')
-    print(msg)
-    return
   source = get_source(msg)
+  print(msg)
+  print(source)
   if source:
-    print(source)
     content = msg['Text']
     if content.startswith(' '):
       print('Replying: ' + content)
       defaultReply = u'不知道该说什么好'
       if survived:
-        reply = get_response(msg['Text'].strip())
+        reply = get_response(content.strip())
         reply = reply or defaultReply
         reply = u'机器人: ' + reply
-        itchat.send(reply, source)
-  print(msg)
+        if DEBUG:
+          print('Sending {} to {}'.format(reply, source))
+        else:
+          itchat.send(reply, source)
+  else:
+    print('Ignored')
 
 
-@itchat.msg_register([itchat.content.PICTURE])
+@itchat.msg_register([itchat.content.PICTURE], isGroupChat=True, isFriendChat=True)
 def get_pic(msg):
-  if msg.get('ActualNickName', None) not in trigger_nicknames and msg.get('FromUserName', None) not in trigger_usernames:
-    print('Ignoring this message')
-    print(msg)
-    return
   source = get_source(msg)
+  print(msg)
   print(source)
   if source:
     filename = msg['FileName']
@@ -93,12 +92,16 @@ def get_pic(msg):
       if info:
         print('output: ' + info)
         if survived:
-          itchat.send(u'机器人: ' + info, source)
+          reply = u'机器人: ' + info
+          if DEBUG:
+            print('Sending {} to {}'.format(reply, source))
+          else:
+            itchat.send(reply, source)
     except:
       print('failed somewhere')
     # itchat.send_image('tmp.jpg', source)
-  print(msg)
-
+  else:
+    print('Ignored')
 
 itchat.auto_login(hotReload=True)
 itchat.run()
